db9a9e2aeadd2f59a86190af615003adbd8298b8
Tostadora-Pensante <jdmullerc@uamv.edu.ni>
2026-09-23 09:55:03 -0600

    actualizo los archivos

--- arreglo1.py
+++ arreglo1.py
@@ -3,7 +3,17 @@ vector.append(2)
 vector.append(5)
 vector.append(9)
 print(vector)
+#To show the size of the vector.
+print("Size of each vector")
 print(len(vector))
-
+#Para mostrar el doble de cada elemento
+print("EL doble de cada elemento")
 for i in range(len(vector)):
-    print(vector[i]*2,  end = " ")
+    if i !=(len(vector)-1):
+        print(vector[i]*2,  end = ", ")
+else:
+    print(f"{vector[i]*2}.")
+
+#agregar a la posicion 1
+vector.insert(1, 36)
+print (vector)
